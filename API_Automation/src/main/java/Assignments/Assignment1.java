e8ec367afd8e8cee78019922e1b8c64ea5276fc1
silpasukumaran1991 <148423011+silpasukumaran1991@users.noreply.github.com>
2023-11-18 08:27:51 +0530

    Upload my api project

---
+++ API_Automation/src/main/java/Assignments/Assignment1.java
@@ -0,0 +1,32 @@
+package Assignments;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Assignment1 {
+	
+	
+	public static HashMap<Character, Integer> countOccurance(String str) {
+		
+		Map<Character, Integer> hashMap = new HashMap<Character,Integer>();
+
+		
+		for(int i=0;i<str.length();i++) {
+			char ch =str.charAt(i);
+			if(ch!=' ') {
+				if(!(hashMap.containsKey(ch))) {
+					hashMap.put(ch, 1);
+				}
+				else {
+					hashMap.put(ch, hashMap.get(ch)+1);
+				}
+			
+			}
+		
+		}
+		
+		return (HashMap<Character, Integer>) hashMap;
+		
+	}
+
+}
---
+++ API_Automation/src/test/java/AssignmentTest/Assignment1Tester.java
@@ -0,0 +1,32 @@
+package AssignmentTest;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import Assignments.Assignment1;
+
+public class Assignment1Tester {
+	
+	@Test
+	public void testOccuranceOfACharacter() {
+		
+		String str="AA";
+		Map<Character,Integer> hashMapExpected= new HashMap<Character,Integer>();
+		
+		hashMapExpected.put('A',2);
+		
+		
+		Map<Character,Integer> hashMapActual= new HashMap<Character,Integer>();
+		hashMapActual=Assignment1.countOccurance(str);
+		
+		Assert.assertEquals(hashMapExpected, hashMapActual);
+		
+		
+		
+		
+	}
+	
+
+}
